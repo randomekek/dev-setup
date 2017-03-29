85183f94d4555060398a7b954113344950dd1ef8
randomekek <davidyang102@gmail.com>
2017-03-29 14:22:42 +1100

    truecolor

--- colors.sh
+++ colors.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 NONE="\033[0m"
- 
-for COLOR in $(seq 30 40) ; do
-    for STYLE in $(seq 0 1) 4 5 7; do
+
+for COLOR in $(seq 30 48) ; do
+    for STYLE in $(seq 0 7); do
         TAG="\033[${STYLE};${COLOR}m"
-        STR="Color:${COLOR}--Style:${STYLE}"
- 
-        echo -ne "${TAG}${STR}${NONE}  "
+        STR="${COLOR}/${STYLE}"
+        echo -ne "${TAG}${STR}${NONE} "
     done
     echo
 done
+
+for G in $(seq 0 30 255) 255 ; do
+  FG="\033[38;2;0;${G};0m"
+  echo -ne "${FG}${G}${NONE} "
+done
+echo
+
+for R in $(seq 0 30 255) 255 ; do
+  BG="\033[48;2;${R};0;0m"
+  echo -ne "${BG}${R}${NONE} "
+done
+echo
